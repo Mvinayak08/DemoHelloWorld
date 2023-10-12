6ac78fa0c1d1b40b3f1095cace87e1b397777cfd
Mvinayak08 <vinayakmahajan08@gmail.com>
2023-10-12 09:35:34 +0530

    final commit

--- DemoProject/src/com/demo/HelloWorld.java
+++ DemoProject/src/com/demo/HelloWorld.java
@@ -3,6 +3,8 @@ package com.demo;
 public class HelloWorld {
 
 	public static void main(String[] args) {
+		System.out.println("This is a demo project to check gitbash and github");
 		System.out.println("Hello World");
+		System.out.println("final commit");
 	}
 }
